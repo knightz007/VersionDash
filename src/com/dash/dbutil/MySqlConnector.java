eabef5a4014d8fdf2b7492cf2c7124252bd0ded6
knightz007 <anupkumar.menon@yahoo.com>
2017-11-30 23:41:19 -0800

    Adding property files

--- src/com/dash/dbutil/MySqlConnector.java
+++ src/com/dash/dbutil/MySqlConnector.java
@@ -3,19 +3,70 @@ package com.dash.dbutil;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class MySqlConnector {
 	
 	public static Connection getMySQLConnection()
 	        throws ClassNotFoundException, SQLException {
-	  
+		
+		String hostName = "";
+	    String dbName = "";
+	    String userName = "";
+	    String password = "";
+	    Properties prop = new Properties();
+		InputStream input = null;
+		try
+		{
+		
+		
+			input = new FileInputStream("config/dbconfig.properties");
+
+
+		// load a properties file
+		 
+		 
+		 
+		 prop.load(input);
+/*		 prop.getProperty("hostname");
+		 prop.getProperty("database");
+		 prop.getProperty("dbuser");
+		 prop.getProperty("dbpassword"); */
+		 
+		   hostName = prop.getProperty("hostName").toString();
+		   dbName =  prop.getProperty("dbName").toString();
+		   userName = prop.getProperty("dbUserName").toString();
+		   password = prop.getProperty("dbPassword").toString();
+		    
+		}
+		catch(IOException  ex)
+		{			
+			System.out.println(ex.getMessage());
+		}
+		catch(Exception e)
+		{
+			System.out.println("There was an issue");
+		}finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return getMySQLConnection(hostName, dbName, userName, password);
 	    // Note: Change the connection parameters accordingly.
-	    String hostName = "localhost";
+/*	    String hostName = "localhost";
 	    String dbName = "testdb";
 	    String userName = "root";
-	    String password = "mysql";
-	    return getMySQLConnection(hostName, dbName, userName, password);
+	    String password = "mysql";*/
+	    //return getMySQLConnection(hostName, dbName, userName, password); 
 	}
 	
 	public static Connection getMySQLConnection(String hostName, String dbName,
