d7702250b40b0cbcfba60ca93491b0c88dea396b
knightz007 <anupkumar.menon@yahoo.com>
2017-12-12 08:14:29 -0800

    Externalize db configs

--- src/com/dash/apputil/LoadProp.java
+++ src/com/dash/apputil/LoadProp.java
@@ -1,6 +1,7 @@
 package com.dash.apputil;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -12,23 +13,37 @@ public class LoadProp {
 
 		Properties prop = new Properties();
 		InputStream input = null;
+		String propFileName="dbConfig.properties";
+		
 		
 		try {
-
-			input = new FileInputStream("config/dbconfig.properties");
-
+				
+			input = LoadProp.class.getClassLoader().getResourceAsStream(propFileName);
+			
+			//input = new FileInputStream("C:\\Users\\anupk\\workspace\\VersionDash\\config\\dbConfig.properties");
+			
+			//input = new FileInputStream("dbConfig.properties");
+			
 			// load a properties file
-			prop.load(input);
+			//prop.load(input);
+			
+
+			if (input != null) {
+				prop.load(input);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
 
 			// get the property value and print it out
 			System.out.println(prop.getProperty("dbName"));
 			System.out.println(prop.getProperty("dbUserName"));
 			System.out.println(prop.getProperty("dbPassword"));
+			System.out.println(System.getProperty("user.dir"));
 
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
-			if (input != null) {
+		if (input != null) {
 				try {
 					input.close();
 				} catch (IOException e) {
--- src/com/dash/dbutil/MySqlConnector.java
+++ src/com/dash/dbutil/MySqlConnector.java
@@ -4,69 +4,48 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+ 
 
 public class MySqlConnector {
 	
 	public static Connection getMySQLConnection()
 	        throws ClassNotFoundException, SQLException {
-		
-		String hostName = "";
-	    String dbName = "";
-	    String userName = "";
-	    String password = "";
-	    Properties prop = new Properties();
-		InputStream input = null;
-		try
-		{
-		
-		
-			input = new FileInputStream("config/dbconfig.properties");
-
-
-		// load a properties file
-		 
-		 
-		 
-		 prop.load(input);
-/*		 prop.getProperty("hostname");
-		 prop.getProperty("database");
-		 prop.getProperty("dbuser");
-		 prop.getProperty("dbpassword"); */
-		 
-		   hostName = prop.getProperty("hostName").toString();
-		   dbName =  prop.getProperty("dbName").toString();
-		   userName = prop.getProperty("dbUserName").toString();
-		   password = prop.getProperty("dbPassword").toString();
-		    
-		}
-		catch(IOException  ex)
-		{			
-			System.out.println(ex.getMessage());
-		}
-		catch(Exception e)
-		{
-			System.out.println("There was an issue");
-		}finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		return getMySQLConnection(hostName, dbName, userName, password);
+	  
 	    // Note: Change the connection parameters accordingly.
 /*	    String hostName = "localhost";
 	    String dbName = "testdb";
 	    String userName = "root";
 	    String password = "mysql";*/
-	    //return getMySQLConnection(hostName, dbName, userName, password); 
+	    
+	    String connectionURL = "";
+	    String userName = "";
+	    String password = "";
+	   
+	 // that's everything from the context.xml and from the global configuration
+	    try
+	    {
+/*	     InitialContext ic = new InitialContext();
+	     Context xmlContext = (Context) ic.lookup("java:comp/env");	    	
+	     String hostNametemp = (String) xmlContext.lookup("dbConnection");*/
+	    	InitialContext initialContext = new InitialContext();
+	    	Context environmentContext = (Context) initialContext.lookup("java:/comp/env");
+	    	connectionURL = (String) environmentContext.lookup("myConnectionURL");
+	    	//System.out.println("connectionURL:" + connectionURL);
+	        userName= (String) environmentContext.lookup("dbUserName");
+	        password=(String) environmentContext.lookup("dbPassword");
+	       // System.out.println(username + ":" + pass);
+	     
+	    }
+	    catch(Exception e)
+	    {
+	    	System.out.println(e.getMessage());
+	    }
+	    // DataSource myDatasource = (DataSource) xmlContext.lookup("jdbc/testdb");
+    
+	    // return getMySQLConnection(hostName, dbName, userName, password);
+	    return getMySQLConnection(connectionURL, userName, password);	    
 	}
 	
 	public static Connection getMySQLConnection(String hostName, String dbName,
@@ -83,6 +62,25 @@ public class MySqlConnector {
 	    // URL Connection for MySQL
 	    // Example: jdbc:mysql://localhost:3306/simplehr
 	    String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+	 
+	    Connection conn = DriverManager.getConnection(connectionURL, userName,
+	            password);
+	    return conn;
+	}
+	
+	public static Connection getMySQLConnection(String connectionUrl, String userName, String password) throws SQLException,
+	        ClassNotFoundException {
+	    
+	    // Declare the class Driver for MySQL DB
+	    // This is necessary with Java 5 (or older)
+	    // Java6 (or newer) automatically find the appropriate driver.
+	    // If you use Java> 5, then this line is not needed.
+	    Class.forName("com.mysql.jdbc.Driver");
+	 
+	 
+	    // URL Connection for MySQL
+	    // Example: jdbc:mysql://localhost:3306/simplehr
+	    String connectionURL = connectionUrl;
 	 
 	    Connection conn = DriverManager.getConnection(connectionURL, userName,
 	            password);
